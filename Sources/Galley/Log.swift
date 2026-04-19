9562716a1a08dd97de5a8a934ae53f1a1d44641c
Munehiro Yamamoto <munepixyz@gmail.com>
2026-04-19 14:33:19 +0900

    sidebar: introduce NSSplitViewController skeleton (Step 1)

--- Sources/Galley/Log.swift
+++ Sources/Galley/Log.swift
@@ -4,4 +4,6 @@ enum Log {
     static let file = Logger(subsystem: "com.github.munepi.galley", category: "file")
     static let forwardSearch = Logger(subsystem: "com.github.munepi.galley", category: "forwardSearch")
     static let inverseSearch = Logger(subsystem: "com.github.munepi.galley", category: "inverseSearch")
+    static let sidebar = Logger(subsystem: "com.github.munepi.galley", category: "sidebar")
+    static let pdfinfo = Logger(subsystem: "com.github.munepi.galley", category: "pdfinfo")
 }
